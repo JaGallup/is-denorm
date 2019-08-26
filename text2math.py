75469be7b2418573dd5016dfd4984cd1bddeb746
Þorsteinn Daði Gunnarsson <thorsteinn@ja.is>
2019-08-26 19:15:17 +0000

    Add phone numbers and math problems

---
+++ text2math.py
@@ -0,0 +1,88 @@
+import operator
+import re
+
+from utils import white_space_tokenizer as t
+
+patterns = [
+    #  (keywords, symbol, operator, priority)
+    ("deilt með", "/", operator.truediv, 2),
+    ("sinnum", "*", operator.mul, 2),
+    ("plús", "+", operator.add, 1),
+    ("mínus", "-", operator.sub, 1),
+    ("í veldinu af", "^", operator.pow, 3),
+    ("veldi", "^", operator.pow, 3),
+    #("minna en|minni en", "<", operator.lt, 0),
+    #("stærra en|stærri en", "<", operator.lt, 0),
+    #("jafnt og|sama og|sama sem|samasem", "=", operator.eq, 0),
+    #("ekki jafnt og|ekki sama og|ekki sama sem|ekki samasem", "=", operator.eq, 0),
+]
+
+patterns_dict = dict()
+for words, *rest in patterns:
+    for word in words.split("|"):
+        patterns_dict[word] = rest
+
+number = re.compile(r"^-?[0-9]+$")
+
+
+def parser(tokens, text):
+    i = 0
+    new_string = []
+    after_num = False
+    between = []
+
+    while i < len(tokens):
+        token = tokens[i]
+        is_num = number.match(token)
+        if is_num and not after_num:
+            after_num = True
+            new_string.append(token)
+            i += 1
+        elif token == "mínus":
+            if len(tokens) > i + 1 and number.match(tokens[i+1]):
+                if not after_num:
+                    after_num = True
+                    new_string.append("-" + tokens[i+1])
+                    i += 2
+                elif (after_num and len(between) > 0):
+                    tokens[i + 1] = "-" + tokens[i+1]
+                    i += 1
+                else:
+                    between.append(token)
+                    i += 1
+            else:
+                new_string.append(token)
+                i += 1
+        elif is_num:
+            b = " ".join(between)
+            between = []
+            s = patterns_dict.get(b, [b])[0]
+            new_string.append(s)
+            new_string.append(token)
+            i += 1
+        elif after_num:
+            between.append(token)
+            i += 1
+        else:
+            new_string.append(token)
+            i += 1
+
+    return " ".join(new_string)
+
+
+if __name__ == "__main__":
+    test_strings = [
+        ("1 plús 2", "1 + 2"),
+        ("4 mínus 3", "4 - 3"),
+        ("1 deilt með 10", "1 / 10"),
+        ("5 sinnum 10 deilt með 4 plús 8", "5 * 10 / 4 + 8"),
+        ("mínus 10 sinnum 33", "-10 * 33"),
+        ("mínus 55 deilt með mínus 5", "-55 / -5"),
+    ]
+
+    for s, target in test_strings:
+        try:
+            parsed = parser(t(s), s)
+            assert parsed == target
+        except AssertionError:
+            print("Failed: ", parsed, "not equal to", target)
---
+++ text2phone.py
@@ -0,0 +1,58 @@
+import operator
+import re
+
+from utils import white_space_tokenizer as t
+
+
+number = re.compile(r"^[0-9]+$")
+
+
+def parser(tokens, text):
+    i = 0
+    new_string = []
+    n_string = []
+
+    while i < len(tokens):
+        token = tokens[i]
+        is_num = number.match(token)
+        if is_num:
+            n_string.append(token)
+        else:
+            n = "".join(n_string)
+            if len(n) in [7, 10, 12]:
+                new_string.append(n)
+            else:
+                new_string.extend(n_string)
+            new_string.append(token)
+            n_string = []
+        i += 1
+
+    n = "".join(n_string)
+    if len(n) in [7, 10, 12]:
+        new_string.append(n)
+    else:
+        new_string.extend(n_string)
+
+
+    return " ".join(new_string)
+
+
+if __name__ == "__main__":
+    test_strings = [
+        ("6 6 3 3 6 0 6", "6633606"),
+        ("0 0 3 5 4 6 6 3 3 6 0 6", "003546633606"),
+        ("3 5 4 6 6 3 3 6 0 6", "3546633606"),
+        ("5 8 1 2 3 4 5", "5812345"),
+        ("4 20 2000", "4202000"),
+        ("18 18", "18 18"),  # This need context, can clash with 18:18 the time, so should be left as is
+        ("5 36 36 36", "5363636"),
+        ("númerið er 5 80 8000 ekkert annað", "númerið er 5808000 ekkert annað"),
+        ("hver er með símanúmerið 5 88 55 22", "hver er með símanúmerið 5885522"),
+    ]   
+    
+    for s, target in test_strings:
+        try:
+            parsed = parser(t(s), s)
+            assert parsed == target
+        except AssertionError:
+            print("Failed: ", parsed, "not equal to", target)
