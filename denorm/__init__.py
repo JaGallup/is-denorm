9e148a0960cffcdc8fc52026e4bffe6289ff42a6
Þorsteinn Daði Gunnarsson <thorsteinn@ja.is>
2019-08-27 14:42:35 +0000

    Add basic usage documentation

--- denorm/__init__.py
+++ denorm/__init__.py
@@ -1,1 +1,14 @@
+"""
+Icelandic de-normalizer Library
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+is-denorm is a denormalizer for Icelandic.
+Basic usage:
+	
+	>>> from denorm import denormalize
+	>>> denormalize("hvað er sjö hundruð og tuttugu deilt með fjórum")
+	"hvað er 720 / 4"
+	
+"""
+
 from denormalizer import denormalize
